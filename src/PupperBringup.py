59f764b7d6f55f206d99cbfc6f34205a94538274
Adam Ring <57344340+Aringan0323@users.noreply.github.com>
2023-02-13 18:48:51 -0500

    Update PupperBringup.py

--- src/PupperBringup.py
+++ src/PupperBringup.py
@@ -18,9 +18,8 @@ FILE_DESCRIPTOR = "walking"
 SERIAL_PORT = "/dev/ttyACM0"
 class PupperBringup:
 
-    def __init__(self, flags):
+    def __init__(self):
 
-        self.FLAGS = flags
 
         self.hardware_interface = HardwareInterface.HardwareInterface(port=SERIAL_PORT)
 
@@ -41,32 +40,7 @@ class PupperBringup:
 
     def setup_hardware(self):
         print("Setting up hardware interface")
-        if self.FLAGS.log:
-            today_string = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-            filename = os.path.join(
-                DIRECTORY, FILE_DESCRIPTOR + "_" + today_string + ".csv"
-            )
-            self.log_file = open(filename, "w")
-            self.hardware_interface.write_logfile_header(self.log_file)
 
-        if self.FLAGS.zero:
-            self.hardware_interface.set_joint_space_parameters(0, 4.0, 4.0)
-            self.hardware_interface.set_actuator_postions(np.zeros((3,4)))
-            input(
-                "Do you REALLY want to calibrate? Press enter to continue or ctrl-c to quit."
-            )
-            print("Zeroing motors...", end="")
-            self.hardware_interface.zero_motors()
-            self.hardware_interface.set_max_current_from_file()
-            print("Done.")
-        else:
-            print("Not zeroing motors!")
-
-        if self.FLAGS.home:
-            print("Homing motors...", end="", flush=True)
-            self.hardware_interface.home_motors()
-            time.sleep(5)
-            print("Done.")
         rospy.sleep(0.1)
         self.hardware_interface.serial_handle.reset_input_buffer()
         rospy.sleep(0.1)
@@ -94,12 +68,6 @@ class PupperBringup:
     def run(self):
         while not rospy.is_shutdown():
             rospy.sleep(0.01)
-            if self.FLAGS.log:
-                any_data = self.hardware_interface.log_incoming_data(self.log_file)
-            # if self.joint_angles is not None:
-            #     self.hardware_interface.set_actuator_positions(self.joint_angles)
-            # elif self.foot_positions is not None:
-            #     self.hardware_interface.set_cartesian_positions(self.foot_positions)
         print("Deactivating Robot")
         rospy.sleep(0.1)
         self.hardware_interface.deactivate()
@@ -107,16 +75,9 @@ class PupperBringup:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--zero", help="zero the motors", action="store_true")
-    parser.add_argument("--log", help="log pupper data to file", action="store_true")
-    parser.add_argument("--home", help="home the motors (moves the legs)", action="store_true")
-    parser.add_argument("--verbose", help="prints relevant data throughout operation", action="store_true")
-
-    FLAGS = parser.parse_args()
     
     rospy.init_node("pupper_bringup")
-    pupper_bringup = PupperBringup(FLAGS)
+    pupper_bringup = PupperBringup()
 
     pupper_bringup.run()
 
