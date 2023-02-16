e3fca220f03d705c9c7846ff4d948bb938803a40
adamring <adamring@brandeis.edu>
2023-02-16 12:20:40 -0500

    fixed camera

--- src/CameraBringup.py
+++ src/CameraBringup.py
@@ -4,20 +4,15 @@ import cv2
 import numpy as np
 import rospy
 from sensor_msgs.msg import CompressedImage
+from utils.bridge import numpy_to_imgmsg
 
 
 
 
-def numpy_to_imgmsg(im):
-    msg = CompressedImage()
-    msg.header.stamp = rospy.Time.now()
-    msg.format = "jpeg"
-    msg.data = np.array(cv2.imencode('.jpg', im)[1]).tobytes()
-    return msg
 
 rospy.init_node("pupper_camera_bringup")
 cam = cv2.VideoCapture(0)
-img_pub = rospy.Publisher("/image/compressed", Image, queue_size=10)
+img_pub = rospy.Publisher("/image/compressed", CompressedImage, queue_size=10)
 rate = rospy.Rate(30)
 
 
