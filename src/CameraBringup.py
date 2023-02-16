11ddaeb4eb9e5fb0512aefd555ac97472dbb3390
adamring <adamring@brandeis.edu>
2023-02-16 11:10:59 -0500

    test camera output shape

--- src/CameraBringup.py
+++ src/CameraBringup.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
 
 import cv2
-import numpy
+import numpy as np
 import rospy
 from sensor_msgs.msg import Image
-import time
 
 
+
+
+def numpy_to_imgmsg(im):
+    msg = Image()
+    msg.header.stamp = rospy.Time.now()
+    msg.height = im.shape[0]
+    msg.width = im.shape[1]
+    msg.encoding = "rgb8"
+    msg.is_bigendian = False
+    msg.step = 3 * msg.width
+    msg.data = im.tobytes()
+    return msg
+
+rospy.init_node("pupper_camera_bringup")
 cam = cv2.VideoCapture(0)
 img_pub = rospy.Publisher("/image/raw", Image, queue_size=10)
-# rate = rospy.Rate(30)
-
-# def numpy_to_imgmsg(arr):
-#     msg = Image()
-#     msg.header.stamp = rospy.Time.now()
-#     msg.height = arr.shape[1]
-#     msg.width = im.width
-#     msg.encoding = "rgb8"
-#     msg.is_bigendian = False
-#     msg.step = 3 * im.width
-#     msg.data = np.array(im).tobytes()
-#     pub.publish(msg)
-
+rate = rospy.Rate(30)
 
 
 while not rospy.is_shutdown():
-    # rate.sleep()
-    time.sleep(0.1)
+    rate.sleep()
     ret, image = cam.read()
-    # img_pub.publish(image)
-    print(image.shape)
+    img_pub.publish(numpy_to_imgmsg(image))
 
 cam.release()
