9de805a7ce595a720e74ec1fce5eff2d86178a0d
adamring <adamring@brandeis.edu>
2023-02-16 10:11:47 -0500

    Testing for camera bringup

---
+++ src/CameraBringup.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy
+import rospy
+from sensor_msgs.msg import CompressedImage
+
+cam = cv2.VideoCapture(0)
+img_pub = rospy.Publisher("/image/compressed", CompressedImage, queue_size=10)
+
+
+while not rospy.is_shutdown():
+    ret, image = cam.read()
+    print(type(image))
