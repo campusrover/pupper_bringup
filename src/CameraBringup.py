27aeb780f5b37d38f99bf723845654ac33da1187
adamring <adamring@brandeis.edu>
2023-02-16 10:51:21 -0500

    Added ros publisher to camera bringup

--- src/CameraBringup.py
+++ src/CameraBringup.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 import cv2
+from cv_bridge import CvBridge
 import numpy
 import rospy
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
 
+bridge = CvBridge()
 cam = cv2.VideoCapture(0)
-img_pub = rospy.Publisher("/image/compressed", CompressedImage, queue_size=10)
+img_pub = rospy.Publisher("/image/raw", Image, queue_size=10)
+rate = rospy.Rate(30)
 
 
 while not rospy.is_shutdown():
+    rate.sleep()
     ret, image = cam.read()
-    print(type(image))
+    img_pub.publish(bridge.cv2_to_imgmsg(image))
 
 cam.release()
