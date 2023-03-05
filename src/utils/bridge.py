3ffeab00f6810921eac57139219110c048449b26
adamring <adamring@brandeis.edu>
2023-03-05 16:08:32 -0500

    point cloud depth publisher

--- src/utils/bridge.py
+++ src/utils/bridge.py
@@ -25,19 +25,21 @@ def numpy_to_pcmsg(depth, amplitude):
     pc_msg.points = [Point32()]*43200
     pc_msg.channels = [ChannelFloat32()]
     pc_msg.channels[0].values = [0]*43200
+    pos=0
     for row_idx in range(180):
         for col_idx in range(240):
             if amplitude[row_idx][col_idx]> 30:
                 zz = depth[row_idx][col_idx]
-                pc_msg.points[col_idx].x = (((120 - col_idx)) / fx) * zz
-                pc_msg.points[col_idx].y = ((90 - row_idx) / fy) * zz
-                pc_msg.points[col_idx].z = zz
-                pc_msg.channels[0].values[col_idx] = depth[row_idx][col_idx]
+                pc_msg.points[pos].x = (((120 - col_idx)) / fx) * zz
+                pc_msg.points[pos].y = ((90 - row_idx) / fy) * zz
+                pc_msg.points[pos].z = zz
+                pc_msg.channels[0].values[pos] = depth[row_idx][col_idx]
             else:
-                pc_msg.points[col_idx].x = 0
-                pc_msg.points[col_idx].y = 0
-                pc_msg.points[col_idx].z = 0
-                pc_msg.channels[0].values[col_idx] = 0
+                pc_msg.points[pos].x = 0
+                pc_msg.points[pos].y = 0
+                pc_msg.points[pos].z = 0
+                pc_msg.channels[0].values[pos] = 0
+            pos += 1
     pc_msg.header.stamp = rospy.Time.now()
     pc_msg.header.frame_id = "pointcloud"
     return pc_msg
