e070d18a458bd62f1032afd3ad55a125aaf6cba8
adamring <adamring@brandeis.edu>
2023-03-07 13:18:09 -0500

    depth cam

--- src/utils/bridge.py
+++ src/utils/bridge.py
@@ -4,20 +4,12 @@ import cv2
 from sensor_msgs.msg import Image, CompressedImage, PointCloud, ChannelFloat32
 from geometry_msgs.msg import Point32
 
-fx = 240 / (2 * np.tan((0.5 * np.pi) * (64.3 / 180)))
-fy = 180 / (2 * np.tan((0.5 * np.pi) * (50.4 / 180)))
 
 
-def get_row_col_arrays(rows:int, cols:int):
-    row_arr = np.tile(np.arange(rows), (cols, 1)).T
-    col_arr = np.tile(np.arange(cols), (rows, 1))
-    return row_arr, col_arr
-
-col_arr = np.tile(np.arange(180), (240, 1)).T
-row_arr = np.tile(np.arange(240), (180, 1))
 
 
-def numpy_to_imgmsg(im):
+
+def numpy_to_compressed_imgmsg(im):
     msg = CompressedImage()
     msg.header.stamp = rospy.Time.now()
     msg.format = "jpeg"
@@ -29,26 +21,3 @@ def imgmsg_to_numpy(msg):
     np_arr = np.fromstring(msg.data, np.uint8)
     return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-def numpy_to_pcmsg(depth, amplitude):
-    pc_msg = PointCloud()
-    pc_msg.points = [Point32()]*43200
-    pc_msg.channels = [ChannelFloat32()]
-    pc_msg.channels[0].values = [0]*43200
-    pos=0
-    for row_idx in range(180):
-        for col_idx in range(240):
-            if amplitude[row_idx][col_idx]> 30:
-                zz = depth[row_idx][col_idx]
-                pc_msg.points[pos].x = (((120 - col_idx)) / fx) * zz
-                pc_msg.points[pos].y = ((90 - row_idx) / fy) * zz
-                pc_msg.points[pos].z = zz
-                pc_msg.channels[0].values[pos] = depth[row_idx][col_idx]
-            else:
-                pc_msg.points[pos].x = 0
-                pc_msg.points[pos].y = 0
-                pc_msg.points[pos].z = 0
-                pc_msg.channels[0].values[pos] = 0
-            pos += 1
-    pc_msg.header.stamp = rospy.Time.now()
-    pc_msg.header.frame_id = "pointcloud"
-    return pc_msg
