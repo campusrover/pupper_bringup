0872acecfd006db4ef002feb344ac6ad008181f8
adamring <adamring@brandeis.edu>
2023-03-09 19:14:03 -0500

    points

--- src/DepthCameraBringup.py
+++ src/DepthCameraBringup.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
             # _, z, x, y = calc.numpy_to_pcmsg(img)
             # point_cloud_pub.publish(calc.msg)
             # rospy.loginfo(img.shape)
-            imgmsg = bridge.cv2_to_imgmsg((depth_buf*1000).astype(np.uint16), encoding="uint16")
+            imgmsg = bridge.cv2_to_imgmsg((depth_buf*1000).astype(np.uint16), encoding="passthrough")
             info.header.stamp = imgmsg.header.stamp
             info_pub.publish(info)
             pub.publish(imgmsg)
